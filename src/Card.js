aa023cb86e24f1eff165732ef8432fdc99892ed7
edge <j2hyeon02@kaist.ac.kr>
2024-07-25 16:43:23 +0900

    feat: checking solved

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,24 @@
 // Card.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './Card.css';
 
-const Card = ({ id, image, title, description, footerLeft, footerRight, onClick }) => {
+const Card = ({ id, image, title, description, footerLeft, onClick, username }) => {
+  const [status, setStatus] = useState(null);
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/users/${username}/check_solved_problem/${id}/`);
+        setStatus(response.data.status);
+      } catch (error) {
+        console.error('Error fetching problem status:', error);
+      }
+    };
+
+    fetchStatus();
+  }, [id, username]);
+
   return (
     <div className="card" onClick={() => onClick(id)}>
       <div className="card-image">
@@ -12,7 +28,9 @@ const Card = ({ id, image, title, description, footerLeft, footerRight, onClick 
       <p>{description}</p>
       <p className="card-footer">
         <span>{footerLeft}</span>
-        <span className="price">{footerRight}</span>
+        <span className={`status ${status ? 'solved' : 'unsolved'}`}>
+          {status ? '해결' : '미해결'}
+        </span>
       </p>
     </div>
   );
